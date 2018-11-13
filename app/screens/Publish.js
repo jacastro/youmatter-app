aa524957b5ef96e042b3e6c8f9577454983738c6
Javier Castro <javier.castro@mercadolibre.com>
2018-11-13 01:05:13 -0300

    change publish screen

--- app/screens/Publish.js
+++ app/screens/Publish.js
@@ -78,68 +78,72 @@ export class PublishScreen extends React.Component {
 
   render() {
     return (
-      <KeyboardAvoidingView>
-        {this.state.loading > 0 && <LoaderScreen
-          overlay
-          backgroundColor={Colors.rgba(Colors.dark80, 0.85)}
-        />}
-        <View useSafeArea margin-20>
-          <View style={styles.buttons} flexGrow column>   
-            <Picker
-              title="Tipo"
-              value={this.state.selectedType}
-              onChange={value => this.setState({selectedType: value})}
-              containerStyle={{marginBottom: 20}}
-            >
-              {this.state.types.map(type => <Picker.Item key={type} value={type} label={type} />)}
-            </Picker>
+      <ScrollView style={{ backgroundColor: "#fff" }}>
+        <KeyboardAvoidingView behavior={"padding"} style={{ padding: 20, backgroundColor: "#fff" }} keyboardVerticalOffset={180}>
+          {this.state.loading > 0 && <LoaderScreen
+            overlay
+            backgroundColor={Colors.rgba(Colors.dark80, 0.85)}
+          />}
+        
+          <View> 
+            <View style={styles.buttons} flexGrow column>   
+              <Picker
+                title="Tipo"
+                value={this.state.selectedType}
+                onChange={value => this.setState({selectedType: value})}
+                containerStyle={{marginBottom: 20}}
+              >
+                {this.state.types.map(type => <Picker.Item key={type} value={type} label={type} />)}
+              </Picker>
 
-            <Picker
-              title="Categoría"
-              placeholder="Categoría"
-              value={this.state.selectedTags}
-              onChange={tags => this.setState({selectedTags: tags})}
-              mode={Picker.modes.MULTI}
-              containerStyle={{marginBottom: 20}}
-            >
-              {this.state.tags.map((tag) => <Picker.Item key={tag} value={tag} label={tag}/>)}
-            </Picker>
+              <Picker
+                title="Categoría"
+                placeholder="Categoría"
+                value={this.state.selectedTags}
+                onChange={tags => this.setState({selectedTags: tags})}
+                mode={Picker.modes.MULTI}
+                containerStyle={{marginBottom: 20}}
+              >
+                {this.state.tags.map((tag) => <Picker.Item key={tag} value={tag} label={tag}/>)}
+              </Picker>
 
-            <TextInput
-              floatingPlaceholder
-              onChangeText={title => this.setState({ title })}
-              floatOnFocus
-              value={this.state.title}
-              placeholder='Título'
-            />
-
-            <View
-              style={{
-                height: 150,
-                borderWidth: 1,
-                padding: 10,
-                borderColor: Colors.dark60,
-              }}
-            >
-              <TextArea 
-                placeholder="Contenido"
-                value={this.state.body}
-                onChangeText={body => this.setState({ body })}
+              <TextInput
+                floatingPlaceholder
+                onChangeText={title => this.setState({ title })}
+                floatOnFocus
+                value={this.state.title}
+                placeholder='Título'
               />
+
+              <View
+                style={{
+                  height: 150,
+                  borderWidth: 1,
+                  padding: 10,
+                  borderColor: Colors.dark60,
+                }}
+              >
+                <TextArea 
+                  placeholder="Contenido"
+                  value={this.state.body}
+                  onChangeText={body => this.setState({ body })}
+                />
+              </View>
+
+              
             </View>
 
-            
+            <Button
+              backgroundColor={Colors.green40}
+              label="Guardar"
+              enableShadow
+              onPress={this.onPublish}
+              marginT-20
+            />
+
           </View>
-
-          <Button
-            backgroundColor={Colors.green40}
-            label="Guardar"
-            enableShadow
-            onPress={this.onPublish}
-          />
-
-        </View>
-      </KeyboardAvoidingView> 
+        
+      </KeyboardAvoidingView> </ScrollView>
     )
   }
 }
@@ -148,13 +152,8 @@ const styles = StyleSheet.create({
   buttons: {
     borderRadius: 10,
     margin: 8,
-    backgroundColor: '#F6F6F6',
-    shadowOpacity: 0.75,
-    shadowRadius: 10,
-    shadowColor: '#999',
     borderRadius: 10,
-    shadowOffset: { height: 10, width: 0 },
-    padding: 15
+    padding: 0
   },
   icon: {
     margin: 'auto',
